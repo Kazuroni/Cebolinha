28db0f37a15b34e808164d128602160c525c85ea
Kazuroni <sayajinbr30@gmail.com>
2021-08-30 01:46:51 -0300

    Finalização

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Cebola;
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+            Console.WriteLine("Oi!, eu sou o Cebolinha.. \nE vou lepetir tudo que você Escleve aqui: ");
+Console.ResetColor();
+            Cebola = Console.ReadLine();
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+            string textocbl = Cebola.Replace("r" , "l").Replace("R" , "L");
+            Console.WriteLine(""); 
+            Console.WriteLine($" {textocbl}");
+Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("Pressione qualquer tecla para finalizar...");
+            Console.ReadKey();
+Console.ResetColor();
+            
+        }
+    }
+}
+
+
+
